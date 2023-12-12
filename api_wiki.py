de4680ddd435d86d31ecc582086b5b4e6226444b
NieWong <ab.sukhee@gmail.com>
2023-12-12 11:11:22 +0800

    chatbot_wiki

---
+++ api_wiki.py
@@ -0,0 +1,24 @@
+# api_wiki.py
+
+import requests
+
+def get_wikipedia_summary(title, lang='en'):
+    base_url = 'https://{lang}.wikipedia.org/w/api.php'.format(lang=lang)
+    params = {
+        'action': 'query',
+        'format': 'json',
+        'prop': 'extracts',
+        'exintro': True,
+        'titles': title,
+    }
+
+    response = requests.get(base_url, params=params)
+    data = response.json()
+
+    # Extract page content from the response
+    pages = data['query']['pages']
+    page_id = next(iter(pages))
+    if 'extract' in pages[page_id]:
+        return pages[page_id]['extract']
+    else:
+        return "No information found."
--- chat.py
+++ chat.py
@@ -1,105 +1,57 @@
-from PyQt5.QtGui import QTextCursor
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QLineEdit, QPushButton, QVBoxLayout, QWidget
-from model import NeuralNet
-from nltkUtils import bag_of_words, tokenize
-import torch
 import random
 import json
-import os
+import torch
+from model import NeuralNet
+from nltkUtils import bag_of_words, tokenize
+from api_wiki import get_wikipedia_summary  # Import the Wikipedia API function
 
-from training import all_words, intents as training_intents, intents
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-os.environ["QT_QPA_PLATFORM"] = "xcb"
+with open('intents.json', 'r') as json_data:
+    intents = json.load(json_data)
 
-class ChatbotGUI(QMainWindow):
-    def __init__(self):
-        super().__init__()
+FILE = "data.pth"
+data = torch.load(FILE)
 
-        self.setWindowTitle("Chatbot GUI")
-        self.setGeometry(100, 100, 600, 400)
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
 
-        self.central_widget = QWidget()
-        self.setCentralWidget(self.central_widget)
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
 
-        self.layout = QVBoxLayout()
+bot_name = "Ivan"
+print("Let's chat! (type 'quit' to exit)")
+while True:
+    sentence = input("You: ")
+    if sentence.lower() == "quit":
+        break
 
-        self.chat_history = QTextEdit(self)
-        self.chat_history.setReadOnly(True)
-        self.layout.addWidget(self.chat_history)
-
-        self.user_input = QLineEdit(self)
-        self.layout.addWidget(self.user_input)
-
-        self.send_button = QPushButton("Send", self)
-        self.send_button.clicked.connect(self.send_message)
-        self.layout.addWidget(self.send_button)
-
-        self.central_widget.setLayout(self.layout)
-
-        # Initialize chatbot components
-        self.initialize_chatbot()
-
-    def initialize_chatbot(self):
-        # Load intents and model
-        with open('intents.json', 'r') as f:
-            intents = json.load(f)
-
-        FILE = "data.pth"
-        data = torch.load(FILE)
-
-        input_size = data["input_size"]
-        hidden_size = data["hidden_size"]
-        output_size = data["output_size"]
-        all_words = data['all_words']
-        self.tags = data['tags']  # Moved 'self.tags' to __init__
-        model_state = data["model_state"]
-
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-        self.model = NeuralNet(input_size, hidden_size, output_size).to(self.device)
-        self.model.load_state_dict(model_state)
-        self.model.eval()
-
-        self.bot_name = "Sam"
-
-    def send_message(self):
-        user_message = self.user_input.text()
-        self.display_message(f"You: {user_message}")
-
-        # Chatbot processing code
-        sentence = tokenize(user_message)
+    if sentence.lower() == "search wikipedia":
+        print(f"{bot_name}: What topic would you like to search on Wikipedia?")
+        search_query = input("You: ")
+        summary = get_wikipedia_summary(search_query)
+        print(f"{bot_name}: Wikipedia Summary for '{search_query}':\n{summary}")
+    else:
+        sentence = tokenize(sentence)
         X = bag_of_words(sentence, all_words)
         X = X.reshape(1, X.shape[0])
-        X = torch.from_numpy(X).to(self.device)
+        X = torch.from_numpy(X).to(device)
 
-        output = self.model(X)
+        output = model(X)
         _, predicted = torch.max(output, dim=1)
 
-        tag = self.tags[predicted.item()]
+        tag = tags[predicted.item()]
 
-        probs = torch.softmax(output, dim=1)
         probs = torch.softmax(output, dim=1)
         prob = probs[0][predicted.item()]
         if prob.item() > 0.75:
             for intent in intents['intents']:
                 if tag == intent["tag"]:
-                    bot_response = random.choice(intent['responses'])
-                    self.display_message(f"{self.bot_name}: {bot_response}")
+                    print(f"{bot_name}: {random.choice(intent['responses'])}")
         else:
-            self.display_message(f"{self.bot_name}: I do not understand...")
-
-        # Clear the user input field
-        self.user_input.clear()
-
-    def display_message(self, message):
-        current_text = self.chat_history.toPlainText()
-        self.chat_history.setPlainText(current_text + message + "\n")
-        self.chat_history.moveCursor(QTextCursor.End)
-
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = ChatbotGUI()
-    window.show()
-    sys.exit(app.exec_())
+            print(f"{bot_name}: I do not understand...")
