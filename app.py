cf9428663027e533eb90a3e3eb0c98adf59e28f3
Ra1toya <21b1num1632@stud.num.edu.mn>
2024-04-22 23:50:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__, template_folder='ui', static_folder='ui')
 
 @app.get("/")
 def index_get():
-    return render_template("base.html")
+    return render_template("index.html")
 
 @app.post("/predict")
 def predict():
